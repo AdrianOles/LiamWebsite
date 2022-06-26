2133d85d221cbc60f7cebf9bc4ee4292176d2195
Adrian Olesniewicz <olesnieadrian@gmail.com>
2022-06-26 12:54:19 -0400

    Added js file for pop-up form

---
+++ app.js
@@ -0,0 +1,19 @@
+//Modal items
+const modal = document.getElementById('email-modal');
+const openBtn = document.querySelector('.main-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+//Click events
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+})
