31b1e8bbc9012276510316b06eac79c3c8b80c1f
Adrian Olesniewicz <olesnieadrian@gmail.com>
2022-07-01 18:31:06 -0400

    Code for Netflix-like carousel

--- app.js
+++ app.js
@@ -23,53 +23,6 @@ window.addEventListener('click', (e) => {
     }
 })
 
-
-// //Form Validation
-// const form = document.getElementById('form');
-// const username = document.getElementById('name');
-// const email = document.getElementById('email');
-// const errorText = document.getElementsByClassName('errorText');
-
-// //Show error message
-// function showError(input, message) {
-//     const formValidation = input.parentElement;
-//     formValidation.className = 'form-validation error';
-
-//     const errorMessage = formValidation.querySelector('p');
-//     errorMessage.innerText = message;
-//     errorText.style.display = 'block';
-// }
-
-// //Show Valid Message
-// function showValid(input) {
-//     const formValidation = input.parentElement;
-//     formValidation.className = 'form-validation valid';
-// }
-
-// //Check Required fields
-// function checkRequired(inputArr) {
-//     inputArr.forEach(function(input) {
-//         if (input.value.trim() === '')
-//         {
-//             showError(input, `${getFieldName(input)} is required`);
-//         } else {
-//             showValid(input);
-//         }
-//     })
-// }
-
-// //Get Field Name
-// function getFieldName(input) {
-//     return input.name.charAt(0).toUpperCase() + input.name.slice(1);
-// }
-
-// //Event Listeners
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     checkRequired([username, email]);
-// })
-
 //Select city
 var x, i, j, l, ll, selElmnt, a, b, c;
 /*look for any elements with the class "custom-select":*/
@@ -249,3 +202,105 @@ dotsNav.addEventListener('click', e => {
     updateDots(currentDot, targetDot);
     hideShowArrows(slides, previousButton, nextButton, targetIndex);
 });
+
+//Netflix Carousel
+const throttleProgressBar = throttle(() => {
+    document.querySelectorAll('.progress-bar').forEach(calculateProgressBar)
+}, 250)
+
+document.addEventListener('click', e => {
+    let handle;
+    if (e.target.matches('.handle')) {
+        handle = e.target
+    } else {
+        handle = e.target.closest('.handle')
+    }
+    if (handle != null) onHandleClick(handle)
+})
+
+window.addEventListener('resize', throttleProgressBar)
+
+document.querySelectorAll('.progress-bar').forEach(calculateProgressBar)
+
+function calculateProgressBar(progressBar) {
+    progressBar.innerHTML = ''
+    const slider = progressBar.closest('.row').querySelector('.slider')
+    const itemCount = slider.children.length
+    const itemsPerScreen = parseInt(
+        getComputedStyle(slider).getPropertyValue('--items-per-screen')
+    )
+    let sliderIndex = parseInt(
+        getComputedStyle(slider).getPropertyValue('--slider-index')
+    )
+    const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen)
+
+    if (sliderIndex >= progressBarItemCount) {
+        slider.style.setProperty('--slider-index', progressBarItemCount - 1)
+        sliderIndex = progressBarItemCount - 1
+    }
+
+    for (let i = 0; i < progressBarItemCount; i++) {
+        const barItem = document.createElement('div')
+        barItem.classList.add('progress-item')
+        if (i === sliderIndex) {
+            barItem.classList.add('active')
+        }
+        progressBar.append(barItem)
+    }
+}
+
+function onHandleClick(handle) {
+    const progressBar = handle.closest('.row').querySelector('.progress-bar')
+    const slider = handle.closest('.container').querySelector('.slider')
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'))
+    const progressBarItemCount = progressBar.children.length
+   
+    if (handle.classList.contains('left-handle')) {
+        if (sliderIndex - 1 < 0) {
+            slider.style.setProperty('--slider-index', progressBarItemCount - 1)
+            progressBar.children[sliderIndex].classList.remove('active')
+            progressBar.children[progressBarItemCount - 1].classList.add('active')
+        } else {
+            slider.style.setProperty('--slider-index', sliderIndex - 1)
+            progressBar.children[sliderIndex].classList.remove('active')
+            progressBar.children[sliderIndex - 1].classList.add('active')
+        }
+    }
+
+    if (handle.classList.contains('right-handle')) {
+        if (sliderIndex + 1 >= progressBarItemCount) {
+            slider.style.setProperty('--slider-index', 0)
+            progressBar.children[sliderIndex].classList.remove('active')
+            progressBar.children[0].classList.add('active')
+        } else {
+            slider.style.setProperty('--slider-index', sliderIndex + 1)
+            progressBar.children[sliderIndex].classList.remove('active')
+            progressBar.children[sliderIndex + 1].classList.add('active')
+        }
+    }
+}
+
+function throttle(cb, delay = 1000) {
+    let shouldWait = false
+    let waitingArgs
+    const timeoutFunc = () => {
+        if (waitingArgs == null) {
+            shouldWait = false
+        } else {
+            cb(...waitingArgs)
+            waitingArgs = null
+            setTimeout(timeoutFunc, delay)
+        }
+    }
+
+    return (...args) => {
+        if (shouldWait) {
+            waitingArgs = args
+            return
+        }
+
+        cb(...args)
+        shouldWait = true
+        setTimeout(timeoutFunc, delay)
+    }
+}
