f868c73e2d69e559fbb93717853938630b955c83
Adrian Olesniewicz <olesnieadrian@gmail.com>
2022-07-01 18:54:56 -0400

    Slider with accurate images

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 //Modal items
-const modal = document.getElementById('email-modal');
-const contactBtn = document.querySelector('.contact-btn');
-const aboutMeBtn = document.querySelector('.aboutMe');
-const closeBtn = document.querySelector('.close-container');
+const modal = document.getElementById("email-modal");
+const contactBtn = document.querySelector(".contact-btn");
+const aboutMeBtn = document.querySelector(".aboutMe");
+const closeBtn = document.querySelector(".close-container");
 
 //Click events
-contactBtn.addEventListener('click', () => {
-    modal.style.display = 'block';
+contactBtn.addEventListener("click", () => {
+    modal.style.display = "block";
 });
 
-aboutMeBtn.addEventListener('click', () => {
-    modal.style.display = 'block';
+aboutMeBtn.addEventListener("click", () => {
+    modal.style.display = "block";
 })
 
-closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
 });
 
-window.addEventListener('click', (e) => {
+window.addEventListener("click", (e) => {
     if (e.target === modal) {
-        modal.style.display = 'none';
+        modal.style.display = "none";
     }
 })
 
@@ -104,13 +104,13 @@ then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
 
 //Cursor
-const cursor = document.querySelector('.cursor');
+const cursor = document.querySelector(".cursor");
 
-document.addEventListener('mousemove', e => {
+document.addEventListener("mousemove", e => {
     cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
 })
 
-document.addEventListener('click', () => {
+document.addEventListener("click", () => {
     cursor.classList.add("expand");
 
     setTimeout(() => {
@@ -118,164 +118,79 @@ document.addEventListener('click', () => {
     }, 500)
 })
 
-// Carousel
-const track = document.querySelector('.carousel__track');
-const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel__button--right');
-const previousButton = document.querySelector('.carousel__button--left');
-const dotsNav = document.querySelector('.carousel__nav');
-const dots = Array.from(dotsNav.children);
-
-const slideWidth = slides[0].getBoundingClientRect().width;
-
-//Arrange the slides next to one another
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
-};
-
-slides.forEach(setSlidePosition);
-
-const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-    currentSlide.classList.remove('current-slide');
-    targetSlide.classList.add('current-slide');
-};
-
-const updateDots = (currentDot, targetDot) => {
-    currentDot.classList.remove('current-slide');
-    targetDot.classList.add('current-slide');
-};
-
-const hideShowArrows = (slides, previousButton, nextButton, targetIndex) => {
-    if (targetIndex === 0) {
-        previousButton.classList.add('is-hidden');
-        nextButton.classList.remove('is-hidden');
-    } else if (targetIndex === slides.length - 1) {
-        previousButton.classList.remove('is-hidden');
-        nextButton.classList.add('is-hidden');
-    } else {
-        previousButton.classList.remove('is-hidden');
-        nextButton.classList.remove('is-hidden');
-    }
-};
-
-//When I click lift, move slides to the left
-previousButton.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.current-slide');
-    const previousSlide = currentSlide.previousElementSibling;
-    const currentDot = dotsNav.querySelector('.current-slide');
-    const previousDot = currentDot.previousElementSibling;
-    const previousIndex = slides.findIndex(slide => slide === previousSlide);
-
-    moveToSlide(track, currentSlide, previousSlide);
-    updateDots(currentDot, previousDot);
-    hideShowArrows(slides, previousButton, nextButton, previousIndex);
-});
-
-//When I click right, move slides to the right
-nextButton.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.current-slide');
-    const nextSlide = currentSlide.nextElementSibling;
-    const currentDot = dotsNav.querySelector('.current-slide');
-    const nextDot = currentDot.nextElementSibling;
-    const nextIndex = slides.findIndex(slide => slide === nextSlide);
-    
-    moveToSlide(track, currentSlide, nextSlide);
-    updateDots(currentDot, nextDot);
-    hideShowArrows(slides, previousButton, nextButton, nextIndex);
-});
-
-
-//When I click the nav indicators, move to that slide
-dotsNav.addEventListener('click', e => {
-    //What indicator was clicked on?
-    const targetDot = e.target.closest('button');
-
-    if (!targetDot) return;
-    
-    const currentSlide = track.querySelector('.current-slide');
-    const currentDot = dotsNav.querySelector('.current-slide');
-    const targetIndex = dots.findIndex(dot => dot === targetDot)
-    const targetSlide = slides[targetIndex];
-
-    moveToSlide(track, currentSlide, targetSlide);
-    updateDots(currentDot, targetDot);
-    hideShowArrows(slides, previousButton, nextButton, targetIndex);
-});
-
 //Netflix Carousel
-const throttleProgressBar = throttle(() => {
-    document.querySelectorAll('.progress-bar').forEach(calculateProgressBar)
-}, 250)
-
-document.addEventListener('click', e => {
-    let handle;
-    if (e.target.matches('.handle')) {
+document.addEventListener("click", e => {
+    let handle
+    if (e.target.matches(".handle")) {
         handle = e.target
     } else {
-        handle = e.target.closest('.handle')
+        handle = e.target.closest(".handle")
     }
     if (handle != null) onHandleClick(handle)
 })
 
-window.addEventListener('resize', throttleProgressBar)
+const throttleProgressBar = throttle(() => {
+    document.querySelectorAll(".progress-bar").forEach(calculateProgressBar)
+}, 250)
+window.addEventListener("resize", throttleProgressBar)
 
-document.querySelectorAll('.progress-bar').forEach(calculateProgressBar)
+document.querySelectorAll(".progress-bar").forEach(calculateProgressBar)
 
 function calculateProgressBar(progressBar) {
-    progressBar.innerHTML = ''
-    const slider = progressBar.closest('.row').querySelector('.slider')
+    progressBar.innerHTML = ""
+    const slider = progressBar.closest(".row").querySelector(".slider")
     const itemCount = slider.children.length
     const itemsPerScreen = parseInt(
-        getComputedStyle(slider).getPropertyValue('--items-per-screen')
+        getComputedStyle(slider).getPropertyValue("--items-per-screen")
     )
     let sliderIndex = parseInt(
-        getComputedStyle(slider).getPropertyValue('--slider-index')
+        getComputedStyle(slider).getPropertyValue("--slider-index")
     )
     const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen)
 
     if (sliderIndex >= progressBarItemCount) {
-        slider.style.setProperty('--slider-index', progressBarItemCount - 1)
+        slider.style.setProperty("--slider-index", progressBarItemCount - 1)
         sliderIndex = progressBarItemCount - 1
     }
 
     for (let i = 0; i < progressBarItemCount; i++) {
-        const barItem = document.createElement('div')
-        barItem.classList.add('progress-item')
+        const barItem = document.createElement("div")
+        barItem.classList.add("progress-item")
         if (i === sliderIndex) {
-            barItem.classList.add('active')
+            barItem.classList.add("active")
         }
         progressBar.append(barItem)
     }
 }
 
 function onHandleClick(handle) {
-    const progressBar = handle.closest('.row').querySelector('.progress-bar')
-    const slider = handle.closest('.container').querySelector('.slider')
-    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'))
+    const progressBar = handle.closest(".row").querySelector(".progress-bar")
+    const slider = handle.closest(".container").querySelector(".slider")
+    const sliderIndex = parseInt(
+        getComputedStyle(slider).getPropertyValue("--slider-index")
+    )
     const progressBarItemCount = progressBar.children.length
-   
-    if (handle.classList.contains('left-handle')) {
+    if (handle.classList.contains("left-handle")) {
         if (sliderIndex - 1 < 0) {
-            slider.style.setProperty('--slider-index', progressBarItemCount - 1)
-            progressBar.children[sliderIndex].classList.remove('active')
-            progressBar.children[progressBarItemCount - 1].classList.add('active')
+            slider.style.setProperty("--slider-index", progressBarItemCount - 1)
+            progressBar.children[sliderIndex].classList.remove("active")
+            progressBar.children[progressBarItemCount - 1].classList.add("active")
         } else {
-            slider.style.setProperty('--slider-index', sliderIndex - 1)
-            progressBar.children[sliderIndex].classList.remove('active')
-            progressBar.children[sliderIndex - 1].classList.add('active')
+            slider.style.setProperty("--slider-index", sliderIndex - 1)
+            progressBar.children[sliderIndex].classList.remove("active")
+            progressBar.children[sliderIndex - 1].classList.add("active")
         }
     }
 
-    if (handle.classList.contains('right-handle')) {
+    if (handle.classList.contains("right-handle")) {
         if (sliderIndex + 1 >= progressBarItemCount) {
-            slider.style.setProperty('--slider-index', 0)
-            progressBar.children[sliderIndex].classList.remove('active')
-            progressBar.children[0].classList.add('active')
+            slider.style.setProperty("--slider-index", 0)
+            progressBar.children[sliderIndex].classList.remove("active")
+            progressBar.children[0].classList.add("active")
         } else {
-            slider.style.setProperty('--slider-index', sliderIndex + 1)
-            progressBar.children[sliderIndex].classList.remove('active')
-            progressBar.children[sliderIndex + 1].classList.add('active')
+            slider.style.setProperty("--slider-index", sliderIndex + 1)
+            progressBar.children[sliderIndex].classList.remove("active")
+            progressBar.children[sliderIndex + 1].classList.add("active")
         }
     }
 }
