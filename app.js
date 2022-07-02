126eaca56176a28a71751b42276a46a5a1724144
Adrian Olesniewicz <olesnieadrian@gmail.com>
2022-07-01 21:33:19 -0400

    Trying to optimize performance

--- app.js
+++ app.js
@@ -103,20 +103,6 @@ function closeAllSelect(elmnt) {
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
 
-//Cursor
-const cursor = document.querySelector(".cursor");
-
-document.addEventListener("mousemove", e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
-})
-
-document.addEventListener("click", () => {
-    cursor.classList.add("expand");
-
-    setTimeout(() => {
-        cursor.classList.remove("expand");
-    }, 500)
-})
 
 //Netflix Carousel
 document.addEventListener("click", e => {
